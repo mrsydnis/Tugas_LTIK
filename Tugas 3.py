5b961e1a2a192c4bcba7d1c613da21a6463da0de
mrsydnis <densiwara@upi.edu>
2024-11-13 02:56:57 +0700

    menambahkan file tugas 3 yaitu mengukur luas segi

---
+++ Tugas 3.py
@@ -0,0 +1,7 @@
+print("mengukur luas segitiga di python")
+
+tinggi = int(input("Masukan tinggi segitiga(cm): "))
+alas = int(input("Masukan alas segitiga(cm): "))
+luas = int(1 / 2 * alas * tinggi)
+
+print(f"Luas segitiga adalah {luas} cm²")
